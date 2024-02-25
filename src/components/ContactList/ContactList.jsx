b62c2138a0a9b2ab06ab4b11639f162bf2382a05
anyalenzy <anya.dudnik.91@gmail.com>
2024-02-25 21:24:40 +0200

    done ContactList, SearchBox

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,1 +1,12 @@
-export default function ContactList() {}
+import Contact from "../Contact/Contact";
+import css from "./ContactList.module.css";
+
+export default function ContactList({ contacts }) {
+  return (
+    <ul className={css.contactList}>
+      {contacts.map((contact) => (
+        <Contact key={contact.id} contact={contact} />
+      ))}
+    </ul>
+  );
+}
