516ba82ec22ab3beec67672e3e54f73e8c8f5421
anyalenzy <anya.dudnik.91@gmail.com>
2024-02-26 16:06:17 +0200

    done Contact form (whithout logic)

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,1 +1,44 @@
-export default function ContactForm() {}
+import { Formik, Form, Field } from "formik";
+import { useId } from "react";
+import css from "./ContactForm.module.css";
+const initialValues = {
+  name: "",
+  number: "",
+};
+export default function ContactForm() {
+  const nameFieldId = useId();
+  const numberFieldId = useId();
+
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    actions.resetForm();
+  };
+  return (
+    <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+      <Form className={css.formContainer}>
+        <div className={css.fieldContainer}>
+          <label htmlFor={nameFieldId}>Name</label>
+          <Field
+            className={css.formInput}
+            type="text"
+            name="name"
+            id={nameFieldId}
+          />
+        </div>
+        <div className={css.fieldContainer}>
+          <label htmlFor={numberFieldId}>Number</label>
+          <Field
+            className={css.formInput}
+            type="tel"
+            name="number"
+            id={numberFieldId}
+          />
+        </div>
+
+        <button className={css.btnSubmit} type="submit">
+          Add contact
+        </button>
+      </Form>
+    </Formik>
+  );
+}
